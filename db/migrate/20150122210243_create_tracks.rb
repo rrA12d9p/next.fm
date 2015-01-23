32a19e2b9ff65f090de4e02e7a7a154251d80f97
TREY <rkh.mymail@gmail.com>
2015-01-23 10:34:13 -0500

    update migrations

--- db/migrate/20150122210243_create_tracks.rb
+++ db/migrate/20150122210243_create_tracks.rb
@@ -1,9 +1,15 @@
 class CreateTracks < ActiveRecord::Migration
   def change
     create_table :tracks do |t|
-      t.string :artist
-      t.string :album
-      t.integer :soundcloud_user_id
+      t.string :artist_name
+      t.string :album_name
+      t.string :track_name
+      t.string :artwork_url
+      t.string :sc_stream_url
+      t.integer :sc_user_id
+      t.integer :sc_artist_id
+      t.integer :sc_album_id
+      t.integer :sc_track_id
     end
   end
 end
